8e22062b25fdc364354cc4f210c784d1b5e1a7c1
raghulsekar <raghulsekar.in@gmail.com>
2022-07-09 19:18:38 +0100

    Solving Error

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from turtle import clear
 import cv2
-from keras.models import load_model
+from keras.models import load_model 
 import numpy as np
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
